ad16a1eb33178f95aa59c3301947243e5fe405b7
Mohd Shadab <Shadabkhanmnw349@gmail.com>
2026-03-06 22:45:21 +0530

    Update inference.py

--- src/inference.py
+++ src/inference.py
@@ -28,7 +28,7 @@ def parse_arguments():
                         choices=['cross_entropy', 'mse'])
     parser.add_argument('-wd', '--weight_decay', type=float, default=0.0001)
     parser.add_argument('-o', '--optimizer', type=str, default='adam',
-                        choices=['sgd', 'momentum', 'nag', 'rmsprop', 'adam', 'nadam'])
+                        choices=['sgd', 'momentum', 'nag', 'rmsprop'])
     parser.add_argument('-lr', '--learning_rate', type=float, default=0.001)
     parser.add_argument('-b', '--batch_size', type=int, default=64)
     parser.add_argument('-wp', '--wandb_project', type=str, default='da6401-assignment-1')
